68d33fdfdc42cc9f9dbfa396dc23086d53f196ba
Hamdan Salih <hamdansalihpc@gmail.com>
2023-11-01 11:31:58 +0530

    create admin all files

---
+++ account/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from adminpanel.models import CustomUser
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+from providers.models import Provider
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('first_name' , 'last_name' , 'age' , 'email' , 'password', 'username')
+        extra_kwargs = {'password' : {'write_only' : True}}
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password' , None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+class ProviderRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ('username' , 'email' , 'age' , 'education' , 'password')
+        extra_kwargs = {'password' : {'write_only' : True}}
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password' , None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.is_provider = True
+        instance.save()
+        return instance
+    
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        exclude = ['password']
+
+class ProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        exclude = ['password']
