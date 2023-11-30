6a20593ea6600dd05aaea4f153e3a66dcd80566f
Hamdan Salih <hamdansalihpc@gmail.com>
2023-11-30 17:04:45 +0530

    update model and url

--- providers/models.py
+++ providers/models.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 
 
 class Provider(CustomUser):
-    education = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100 , blank=True)
     storage_allocated = models.DecimalField(max_digits=5,decimal_places=1,default=2.0)
     rating = models.FloatField(default=0.0)
--- providers/urls.py
+++ providers/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from .views import UpdateProfile
 
 urlpatterns = [
+    path('profile-update' , UpdateProfile.as_view() , name="prov-update" ),
+    path('profile' , UpdateProfile.as_view() , name="prov-profile" ),
 ]
