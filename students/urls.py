4776788a08326ca67948d44d12e8456357d38251
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-01 17:06:52 +0530

    update view and url

--- students/urls.py
+++ students/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('profile/' , views.profileView , name="stud-profile"),
+    path('delete' , views.deleteAccount , name="stud-delete"),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from adminpanel.models import CustomUser
+from .serializer import StudentProfileSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.permissions import IsAuthenticated , AllowAny
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profileView(request):
+    user = request.user
+    serializer = StudentProfileSerializer(user)
+    print(serializer.data)
+    return Response(data=serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteAccount(request):
+    user = request.user
+    user.delete()
+    print('user deleted')
+    return Response(data={"message" : "account deleted Successfully"})
+    
