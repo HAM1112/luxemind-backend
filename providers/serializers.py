cff9e58b0708a80183b1b61d6b7c4d5ecc1e1d52
Hamdan Salih <hamdansalihpc@gmail.com>
2023-12-09 15:07:56 +0530

    provider serializer

--- providers/serializers.py
+++ providers/serializers.py
@@ -1,12 +1,42 @@
 from rest_framework import serializers
 from adminpanel.models import CustomUser
-from .models import Provider        
+from .models import *       
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Provider
         fields = ('first_name' , "last_name" , 'dob' , 'education' , 'username' , 'email' )
     
-        
-    
-    
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = '__all__'
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+   
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def update(self, instance, validated_data):
+        # Your custom update logic for nested fields
+        nested_data = validated_data.pop('nested_field', None)
+        if nested_data:
+            # Your logic to update nested field
+            pass
+
+        # Continue with the regular update logic for other fields
+        instance = super().update(instance, validated_data)
+
+        return instance
