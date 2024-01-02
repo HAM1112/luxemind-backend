2e05b3bc45fa8843b36eef438087c46147a6e644
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-02 15:26:20 +0530

    account

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import  RegisterView , ProviderRegisterView , MyTokenObtainPairView
+from .views import RegisterView, ProviderRegisterView, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenVerifyView,
     TokenRefreshView,
@@ -7,12 +7,10 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     # path('register/', CustomUserCreateView.as_view(), name='register'),
-    
-    path('studRegister/', RegisterView.as_view() , name='studregister'),
-    path('provRegister/', ProviderRegisterView.as_view() , name='provregister'),
-    
+    path("studRegister/", RegisterView.as_view(), name="studregister"),
+    path("provRegister/", ProviderRegisterView.as_view(), name="provregister"),
     # path('list/', ListCustomUser.as_view() , name='list'),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
--- account/views.py
+++ account/views.py
@@ -3,14 +3,12 @@ from django.contrib.auth import authenticate
 from adminpanel.models import CustomUser
 from providers.models import Provider
 
-from .serializers import  (
-    RegisterSerializer ,
-    ProviderSerializer,    
+from .serializers import (
+    RegisterSerializer,
+    ProviderSerializer,
     ProviderRegisterSerializer,
-    UserSerializer
-    )
-
-
+    UserSerializer,
+)
 
 
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -25,43 +23,50 @@ from rest_framework import generics
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
+        print("starting")
         token = super().get_token(user)
 
         # Add custom claims
-        token['username'] = user.username
-        token['is_superuser'] = user.is_superuser
-        token['is_provider'] = user.is_provider
+        token["username"] = user.username
+        token["is_superuser"] = user.is_superuser
+        token["is_provider"] = user.is_provider
         # ...
+        print("ending")
 
         return token
+
+
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
+
     def post(self, request, *args, **kwargs):
         reg_serilizer = RegisterSerializer(data=request.data)
         if reg_serilizer.is_valid():
-            print('is____________valid')
+            print("is____________valid")
             newuser = reg_serilizer.save()
-            if newuser :
-                print('user is created')
+            if newuser:
+                print("user is created")
                 return Response(status=status.HTTP_201_CREATED)
-        return Response(reg_serilizer.errors , status=status.HTTP_400_BAD_REQUEST)
-    
-    
+        return Response(reg_serilizer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class ProviderRegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
-    def post(self, request , *args, **kwargs):
+
+    def post(self, request, *args, **kwargs):
         reg_serilizer = ProviderRegisterSerializer(data=request.data)
         if reg_serilizer.is_valid():
             print("---------------------provider valid ------------------")
             newprov = reg_serilizer.save()
-            if newprov :
-                print('provider created')
-                return Response(data=reg_serilizer.data ,status=status.HTTP_201_CREATED)
-        return Response(reg_serilizer.errors , status=status.HTTP_400_BAD_REQUEST)
+            if newprov:
+                print("provider created")
+                return Response(data=reg_serilizer.data, status=status.HTTP_201_CREATED)
+        return Response(reg_serilizer.errors, status=status.HTTP_400_BAD_REQUEST)
