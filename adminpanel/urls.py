cfffae44bd108f94a8d2987f665b0a1c98f400c1
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-03 15:31:09 +0530

    adminpanel

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('profile/' ,AdminProfileView , name='admin-profile'),
-    path('profile-update' , UpdateProfileView.as_view() , name='admin-profile-update'),
-    path('all-users/', listAllUsers , name='all-users'),
-    path('update-status', updateUserStatus , name='update-status'),
-    path('delete-user/<int:user_id>', deleteUser , name='delete-user'),
-    path('get_all_courses/' , get_all_courses , name='get_all_courses'),
-    path('get_all_providers/' , get_all_providers , name='get_all_providers'),
-    path('approve_course/<int:course_id>' , approver_course , name='approve_course'),
-    path('get_user_details/<int:user_id>/' , get_user_details , name='user_details'),
+    path("profile/", AdminProfileView, name="admin-profile"),
+    path("profile-update", UpdateProfileView.as_view(), name="admin-profile-update"),
+    path("all-users/", listAllUsers, name="all-users"),
+    path("update-status", updateUserStatus, name="update-status"),
+    path("delete-user/<int:user_id>", deleteUser, name="delete-user"),
+    path("get_all_courses/", get_all_courses, name="get_all_courses"),
+    path("get_all_providers/", get_all_providers, name="get_all_providers"),
+    path("approve_course/<int:course_id>", approver_course, name="approve_course"),
+    path("block_course/<int:course_id>", block_course, name="block_course"),
+    path("get_user_details/<int:user_id>/", get_user_details, name="user_details"),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -1,103 +1,164 @@
 from django.shortcuts import render
 from .models import *
 from .serializers import *
-from rest_framework.decorators import api_view , permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from providers.models import *
 from providers.serializers import *
+from students.serializer import *
+from .tasks import *
 
 
-@api_view(['GET']) 
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def AdminProfileView(request):
     serializer = AdminSerializer(request.user)
     return Response(serializer.data)
 
+
 class UpdateProfileView(APIView):
     permission_classes = [IsAuthenticated]
+
     def put(self, request, *args, **kwargs):
         user = request.user
-        serializer = AdminProfileUpdateSerializer(user, data=request.data , partial=True)
-        print(user.first_name)
+        serializer = AdminProfileUpdateSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
-            print("serializer is valid")
             serializer.save()
-            return Response(data={"message" : "updated Successfully"})
-        else:
-            print('not valid ')
+            return Response(data={"message": "updated Successfully"})
         return Response()
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def listAllUsers(request):
-    users = CustomUser.objects.exclude(pk=request.user.pk).order_by('id')
-    serializer = AdminSerializer(users , many=True)
-    return Response(data=serializer.data , status=status.HTTP_200_OK)
+    users = CustomUser.objects.exclude(pk=request.user.pk).order_by("id")
+    serializer = AdminSerializer(users, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['PUT'])
+
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def updateUserStatus(request):
-    user_id = request.data.get('id')
+    user_id = request.data.get("id")
     user = CustomUser.objects.get(id=user_id)
-    print(user.is_active)
     user.is_active = not user.is_active
-    print(user.username)
-    print(user.is_active)
     user.save()
-    return Response(data={'message' : 'Status changed Successfully'},status=status.HTTP_202_ACCEPTED)
+    return Response(
+        data={"message": "Status changed Successfully"}, status=status.HTTP_202_ACCEPTED
+    )
 
-@api_view(['DELETE'])
+
+@api_view(["DELETE"])
 @permission_classes([IsAuthenticated])
-def deleteUser(request , user_id):
-    print(user_id)
+def deleteUser(request, user_id):
     user = CustomUser.objects.get(id=user_id)
-    user.delete()
-    print('deleting user')
-    users = CustomUser.objects.exclude(pk=request.user.pk).order_by('id')
-    serializer = AdminSerializer(users , many=True)
-    return Response(data=serializer.data , status=status.HTTP_200_OK)
+    users = CustomUser.objects.exclude(pk=request.user.pk).order_by("id")
+    serializer = AdminSerializer(users, many=True)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_all_courses(request):
     courses = Course.objects.all()
-    serializer = CourseSerializer(courses , many=True)
+    serializer = CourseSerializer(courses, many=True)
     return Response(data=serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_all_providers(request):
     providers = Provider.objects.all()
-    serializer = ProviderSerializer(providers , many=True)
+    serializer = ProviderSerializer(providers, many=True)
     return Response(data=serializer.data)
 
-@api_view(['PUT'])
+
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
-def approver_course(request , course_id):
+def approver_course(request, course_id):
     try:
         course = Course.objects.get(pk=course_id)
     except Course.DoesNotExist:
-        return Response({'error': 'Course not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
     if course.is_published:
-        return Response({'error': 'Course is already published'}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response(
+            {"error": "Course is already published"}, status=status.HTTP_400_BAD_REQUEST
+        )
     course.is_pending = False
     course.is_published = True
     course.save()
+    send_mail_to_related_students(course.subject.name, course.name)
     serializer = CourseSerializer(course)
     return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    
-@api_view(['GET'])
+
+
+@api_view(["PUT"])
 @permission_classes([IsAuthenticated])
-def get_user_details(request , user_id):
+def block_course(request, course_id):
+    try:
+        course = Course.objects.get(pk=course_id)
+    except Course.DoesNotExist:
+        return Response({"error": "Course not found"}, status=status.HTTP_404_NOT_FOUND)
+    if not course.is_published:
+        return Response(
+            {"error": "Course is already Unpublished"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    course.is_pending = True
+    course.is_published = False
+    course.save()
+    serializer = CourseSerializer(course)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user_details(request, user_id):
     user = CustomUser.objects.get(id=user_id)
-    if user.is_provider :
+    if user.is_provider:
         user = Provider.objects.get(id=user_id)
         serializer = ProviderSerializer(user)
-        return Response(serializer.data)
+        data = {
+            "user": serializer.data,
+        }
+        return Response(data)
+
     serializer = UserSerializer(user)
-    return Response(serializer.data)
+    enrolled_courses = Course.objects.filter(purchase__student_id=user_id)
+    payments = PaymentDetails.objects.filter(purchase__student=user)
+    certifeid = Purchase.objects.filter(student=user, certified=True)
+
+    all_payments = []
+    for payment in payments:
+        detials = {
+            "payement": PayementDetailsSerializer(payment).data,
+            "course": {
+                "course_id": payment.purchase.course.id,
+                "course_name": payment.purchase.course.name,
+                "provider_id": payment.purchase.course.provider.id,
+                "provider_name": payment.purchase.course.provider.username,
+            },
+        }
+        all_payments.append(detials)
+
+    data = {
+        "user": serializer.data,
+        "courses": CourseSerializer(enrolled_courses, many=True).data,
+        "payments": all_payments,
+        "certificates": PurchaseSerializer(certifeid, many=True).data,
+    }
+    return Response(data=data)
+
+
+def test():
+    print("physics")
+    users = (
+        CustomUser.objects.filter(purchase__course__subject__name="physics")
+        .values_list("email", flat=True)
+        .distinct()
+    )
+    print(users)
+    return "what"
