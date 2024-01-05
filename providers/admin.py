055cdc40bd6ad260990b0a77ea722cbcf183acd2
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-05 15:38:58 +0530

    provider

--- providers/admin.py
+++ providers/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
 
 admin.site.register(Provider)
 admin.site.register(Course)
-admin.site.register(Subject) 
-admin.site.register(Module) 
-admin.site.register(Lesson) 
+admin.site.register(Subject)
+admin.site.register(Module)
+admin.site.register(Lesson)
+admin.site.register(QuizQuestions)
+admin.site.register(Quiz)
