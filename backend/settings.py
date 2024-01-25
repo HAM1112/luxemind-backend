0a4c00a36615845a507d59946072786001ad5101
Hamdan Salih <hamdansalihpc@gmail.com>
2024-01-25 22:43:24 +0530

    cor origin

--- backend/settings.py
+++ backend/settings.py
@@ -149,10 +149,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "*"
-    ]
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
